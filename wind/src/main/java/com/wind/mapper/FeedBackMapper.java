83695527b43da7edc4617714963cb276053b2f8e
ml <1161089366@qq.com>
2022-05-24 00:16:38 +0800

    映射

---
+++ wind/src/main/java/com/wind/mapper/FeedBackMapper.java
@@ -0,0 +1,30 @@
+package com.wind.mapper;
+
+import com.wind.entity.FeedBack;
+import com.wind.entity.FeedBackExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface FeedBackMapper {
+    int countByExample(FeedBackExample example);
+
+    int deleteByExample(FeedBackExample example);
+
+    int deleteByPrimaryKey(Integer fId);
+
+    int insert(FeedBack record);
+
+    int insertSelective(FeedBack record);
+
+    List<FeedBack> selectByExample(FeedBackExample example);
+
+    FeedBack selectByPrimaryKey(Integer fId);
+
+    int updateByExampleSelective(@Param("record") FeedBack record, @Param("example") FeedBackExample example);
+
+    int updateByExample(@Param("record") FeedBack record, @Param("example") FeedBackExample example);
+
+    int updateByPrimaryKeySelective(FeedBack record);
+
+    int updateByPrimaryKey(FeedBack record);
+}
