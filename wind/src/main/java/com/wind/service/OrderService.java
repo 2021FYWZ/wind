c6c8001dff46ac61066ce14fef68aae43ec86ac1
lfy <1483985774@qq.com>
2022-05-25 17:46:33 +0800

    service层：OrderService

---
+++ wind/src/main/java/com/wind/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.wind.service;
+
+import com.wind.entity.Order;
+import com.wind.entity.OrderExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OrderService {
+    int countByExample(OrderExample example);
+
+    int deleteByExample(OrderExample example);
+
+    int deleteByPrimaryKey(Integer oId);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    List<Order> selectByExample(OrderExample example);
+
+    Order selectByPrimaryKey(Integer oId);
+
+    int updateByExampleSelective(@Param("record") Order record, @Param("example") OrderExample example);
+
+    int updateByExample(@Param("record") Order record, @Param("example") OrderExample example);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
